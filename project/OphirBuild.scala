d5fda41d66f214e7ef082b40f71fdaf75c720ed1
ornicar <thibault.duplessis@gmail.com>
2011-09-29 15:22:12 +0200

    Attempt to add mongodb dependencies; but no Internet access to fetch them now

--- project/OphirBuild.scala
+++ project/OphirBuild.scala
@@ -6,9 +6,16 @@ object OphirBuild extends Build
   lazy val root = Project("root", file(".")) settings(
     name := "Ophir",
     version := "0.1",
-    scalaVersion := "2.9.1",
+    scalaVersion := "2.9.0-1",
     libraryDependencies ++= Seq(
-      "org.scala-lang" % "scala-compiler" % "2.9.1"
+      "org.scala-lang" % "scala-compiler" % "2.9.0-1",
+      "com.mongodb.casbah" %% "casbah" % "2.1.5.0",
+      "org.slf4j" % "slf4j-simple" % "1.6.1" % "runtime",
+      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
+    ),
+    resolvers ++= Seq(
+        "repo.novus rels" at "http://repo.novus.com/releases/",
+        "repo.novus snaps" at "http://repo.novus.com/snapshots/"
     )
   )
 
