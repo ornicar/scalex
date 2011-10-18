5a6ff88ee2940277d878c784b51c92674bf08a33
ornicar <thibault.duplessis@gmail.com>
2011-10-18 14:25:36 +0200

    Refactor main servlet

--- http/src/main/scala/MainServlet.scala
+++ http/src/main/scala/MainServlet.scala
@@ -5,38 +5,34 @@ import org.scalatra._
 import scalex.search.{Search, Query}
 import scalex.model.{Def, Block}
 import collection.mutable.WeakHashMap
-import com.github.ornicar.paginator._
 import javax.servlet.http._
-
-class SalatAdapter[A <: CaseClass](dao: DAO[A, ObjectId], query: MongoDBObject) extends Adapter[A] {
-
-  def nbResults = dao.count(query).toInt
-
-  def slice(offset: Int, length: Int): Seq[A] =
-    dao.find(query).skip(offset).limit(length).toSeq
-}
+import com.github.ornicar.paginator.Paginator
 
 class MainServlet extends ScalatraServlet {
 
-  val limit = 3
+  val limit = 10
 
   val cache = WeakHashMap[Query, String]()
 
   get("/") {
-    contentType = "application/json"
 
-    val response = request.getParameter("q") match {
-      case null | "" => error("Empty query!")
-      case q => {
-        val query = Query(q, request.getParameter("page").toInt, limit)
+    val response = getSome("q") match {
+      case None => error("Empty query!")
+      case Some(q) => {
+        val query = Query(q, page, limit)
         cache.getOrElseUpdate(query, search(query))
       }
     }
 
+    contentType = getSome("callback") match {
+      case None => "application/json"
+      case Some(_) => "application/javascript"
+    }
+
     // handle jsonp
-    request.getParameter("callback") match {
-      case null | "" => response
-      case callback => "%s(%s)" format (callback, response)
+    getSome("callback") match {
+      case None => response
+      case Some(callback) => "%s(%s)" format (callback, response)
     }
   }
 
@@ -44,126 +40,26 @@ class MainServlet extends ScalatraServlet {
     <h1>Not found. Bummer.</h1>
   }
 
-  def page: Int = request.getParameter("page") match {
-    case null | "" | "0" => 1
-    case spage => spage.toInt
+  def getSome(name: String): Option[String] = request.getParameter(name) match {
+    case null | "" => None
+    case x => Some(x)
   }
+
+  def page: Int = getSome("page") map (_.toInt) getOrElse(1)
 
   def error(msg: String): String = JsonObject(Map("error" -> msg)).toString
 
-  def search(query: Query, page: Int): String = Search find query match {
+  def search(query: Query): String = Search find query match {
     case Left(msg) => error(msg)
-    case Right(results) => makeResult(results, page).toString
+    case Right(paginator) => makeResult(query, paginator).toString
   }
 
-  def makeResult(funs: Seq[Def], page: Int): JsonObject = JsonObject(Map(
-    "results" -> (funs map funToJsonObject),
-    "nb" -> funs.size,
-    "page" -> page
+  def makeResult(query: Query, paginator: Paginator[Def]): JsonObject = JsonObject(Map(
+    "query" -> query.string,
+    "results" -> (paginator.currentPageResults.toList map (JsonObject(_))),
+    "nbResults" -> paginator.nbResults,
+    "page" -> page,
+    "nbPages" -> paginator.nbPages
   ))
 
-  def funToJsonObject(fun: Def): JsonObject = {
-
-    def optionBlock(b: Option[Block]): JsonObject =
-      JsonObject("html" -> (b map (_.html)), "txt" -> (b map (_.txt)))
-
-    def block(b: Block): JsonObject =
-      JsonObject("html" -> b.html, "txt" -> b.txt)
-
-    JsonObject(
-      "name" -> fun.name,
-      "qualifiedName" -> fun.qualifiedName,
-      "parent" -> JsonObject(
-        "name" -> fun.parent.name,
-        "qualifiedName" -> fun.parent.qualifiedName,
-        "typeParams" -> fun.parent.showTypeParams
-      ),
-      "comment" -> (fun.comment map { com =>
-        JsonObject(
-          "short" -> block(com.short),
-          "body" -> block(com.body),
-          "authors" -> (com.authors map block),
-          "see" -> (com.see map block),
-          "result" -> optionBlock(com.result),
-          "throws" -> JsonObject(com.throws map { case (k, v) => (k.replace("_", ".") -> block(v)) }),
-          "typeParams" -> JsonObject(com.typeParams map { case (k, v) => (k.replace("_", ".") -> block(v)) }),
-          "valueParams" -> JsonObject(com.valueParams map { case (k, v) => (k.replace("_", ".") -> block(v)) }),
-          "version" -> optionBlock(com.version),
-          "since" -> optionBlock(com.since),
-          "todo" -> (com.todo map block),
-          "note" -> (com.note map block),
-          "example" -> (com.example map block),
-          "constructor" -> optionBlock(com.constructor),
-          "source" -> com.source
-        )
-      }),
-      "typeParams" -> fun.showTypeParams,
-      "resultType" -> fun.resultType,
-      "valueParams" -> fun.paramSignature,
-      "signature" -> fun.signature,
-      "package" -> fun.pack
-    ) removeNones
-  }
-
-  case class JsonObject(pairs: Map[String, Any]) {
-
-    override def toString = pairs map renderPair mkString ("{", ", ", "}")
-
-    def renderPair(pair: (String, Any)): String = pair match {
-      case (k, v) => "\"%s\": %s" format (k, renderValue(v))
-    }
-
-    def renderValue(value: Any): String = value match {
-      case v: JsonObject => v.toString
-      case v: List[_] => v mkString ("[", ", ", "]")
-      case v => quote(v.toString)
-    }
-
-    def removeNones: JsonObject = JsonObject(
-      pairs map {
-        case (k, Some(o: JsonObject)) => (k, o.removeNones)
-        case (k, o: JsonObject) => (k, o.removeNones)
-        case (k, Some(v)) => (k, v)
-        case x => x
-      } filter {
-        case (k, None) => false
-        case (k, Nil) => false
-        case (k, o: JsonObject) if o.pairs.isEmpty => false
-        case x => true
-      }
-    )
-
-    /**
-    * Quote a string according to "JSON rules".
-    */
-    def quote(s: String) = {
-      val charCount = s.codePointCount(0, s.length)
-      "\"" + 0.to(charCount - 1).map { idx =>
-        s.codePointAt(s.offsetByCodePoints(0, idx)) match {
-          case 0x0d => "\\r"
-          case 0x0a => "\\n"
-          case 0x09 => "\\t"
-          case 0x22 => "\\\""
-          case 0x5c => "\\\\"
-          case 0x2f => "\\/" // to avoid sending "</"
-          case c => quotedChar(c)
-        }
-      }.mkString("") + "\""
-    }
-
-    private[this] def quotedChar(codePoint: Int) = {
-      codePoint match {
-        case c if c > 0xffff =>
-          val chars = Character.toChars(c)
-          "\\u%04x\\u%04x".format(chars(0).toInt, chars(1).toInt)
-        case c if c > 0x7e => "\\u%04x".format(c.toInt)
-        case c => c.toChar
-      }
-    }
-  }
-
-  object JsonObject {
-
-    def apply(pairs: (String, Any)*): JsonObject = JsonObject(pairs.toMap)
-  }
 }
