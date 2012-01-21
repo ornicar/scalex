6b757972e0067b8def7c555201794f07b7d14db9
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-21 21:23:41 +0100

    Add some scalaz goodies and the k combinator

---
+++ core/src/main/scala/scalex/package.scala
@@ -0,0 +1,20 @@
+package object scalex
+  extends scalaz.Identitys
+  with    scalaz.Equals
+  with    scalaz.MABs
+  with    scalaz.MAs
+  with    scalaz.Options {
+
+  /**
+   * K combinator implementation
+   * Provides oneliner side effects
+   * See http://hacking-scala.posterous.com/side-effecting-without-braces
+   */
+  implicit def addKcombinator[A](any: A) = new {
+    def kCombinator(sideEffect: A => Unit): A = {
+      sideEffect(any)
+      any
+    }
+    def ~(sideEffect: A => Unit): A = kCombinator(sideEffect)
+  }
+}
