2850abb3e40bfacd07e08c9ba8a0f7fd715ba65e
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-21 21:23:12 +0100

    Refactor the build

--- project/ScalexBuild.scala
+++ project/ScalexBuild.scala
@@ -1,12 +1,24 @@
 import sbt._
 import Keys._
 
+object Resolvers {
+  val twitter = "twitter.com" at "http://maven.twttr.com/"
+  val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
+  val iliaz = "iliaz.com" at "http://scala.iliaz.com/"
+  val novus = "novus" at "http://repo.novus.com/snapshots/"
+}
+
 object ScalexBuild extends Build
 {
+  import Resolvers._
+
   lazy val core = Project("core", file("core")) settings(
     name := "Scalex Core",
     version := "0.1",
     scalaVersion := "2.9.1",
+    scalacOptions += "-deprecation",
+    scalacOptions += "-unchecked",
+    shellPrompt := ShellPrompt.buildShellPrompt,
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-compiler" % "2.9.1",
       "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
@@ -15,13 +27,11 @@ object ScalexBuild extends Build
       "org.scala-tools.testing" %% "scalacheck" % "1.9",
       "org.scala-tools.testing" % "test-interface" % "0.5",
       "org.scalatest" % "scalatest_2.9.0" % "1.6.1",
-      "com.github.ornicar" % "paginator-core_2.9.1" % "1.1",
-      "com.github.ornicar" % "paginator-salat-adapter_2.9.1" % "1.1"
+      "com.github.ornicar" %% "paginator-core" % "1.2",
+      "com.github.ornicar" %% "paginator-salat-adapter" % "1.1",
+      "org.scalaz" %% "scalaz-core" % "6.0.3"
     ),
-    resolvers ++= Seq(
-      "repo.novus rels" at "http://repo.novus.com/releases/",
-      "repo.novus snaps" at "http://repo.novus.com/snapshots/"
-    )
+    resolvers ++= Seq(novus, iliaz)
   )
 
   lazy val http = Project("http", file("http")) dependsOn(core) settings(
@@ -31,16 +41,19 @@ object ScalexBuild extends Build
     libraryDependencies ++= Seq(
       "org.scalatra" %% "scalatra" % "2.0.1",
       "javax.servlet" % "servlet-api" % "2.5" % "provided",
-      "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "jetty"
+      "org.eclipse.jetty" % "jetty-webapp" % "8.0.4.v20111024" % "container"
     ),
-    resolvers ++= Seq(
-      "repo.novus rels" at "http://repo.novus.com/releases/",
-      "repo.novus snaps" at "http://repo.novus.com/snapshots/"
-    )
+    resolvers ++= Seq(novus)
   )
+}
 
-  // append -deprecation to the options passed to the Scala compiler
-  scalacOptions += "-deprecation"
+object ShellPrompt {
 
-  scalacOptions += "-unchecked"
+  val buildShellPrompt = {
+    (state: State) =>
+      {
+        val currProject = Project.extract(state).currentProject.id
+        "hermes:%s> ".format(currProject)
+      }
+  }
 }
