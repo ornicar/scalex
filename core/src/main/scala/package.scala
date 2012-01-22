2da4f06f72c09ba9bb667b3d4fcbe293fd6b121a
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-22 14:05:16 +0100

    Attempt to make salat serialize scalaz NonEmptyList

--- core/src/main/scala/package.scala
+++ core/src/main/scala/package.scala
@@ -4,6 +4,7 @@ package object scalex
   with scalaz.MABs
   with scalaz.MAs
   with scalaz.Options
+  with scalaz.Lists
   with scalaz.Booleans {
 
   /**
@@ -17,5 +18,85 @@ package object scalex
       any
     }
     def ~(sideEffect: A => Unit): A = kCombinator(sideEffect)
+  }
+
+  com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers()
+
+  RegisterScalazConversionHelpers()
+
+}
+
+import _root_.com.mongodb.casbah.commons.conversions.MongoConversionHelper
+import org.bson.{ BSON, Transformer }
+import scalaz._
+
+object RegisterScalazConversionHelpers extends ScalazHelpers {
+  def apply() = {
+    println("Registering Scalaz Conversions.")
+    super.register()
+  }
+}
+
+object DeregisterScalazConversionHelpers extends ScalazHelpers {
+  def apply() = {
+    println("Unregistering Scalaz Conversions.")
+    super.unregister()
+  }
+}
+
+trait ScalazHelpers extends ScalazSerializer with ScalazDeserializer
+
+trait ScalazSerializer extends MongoConversionHelper {
+
+  private val encodeType = classOf[NonEmptyList[_]]
+
+  /** Encoding hook for MongoDB To be able to persist Scalaz DateTime to MongoDB */
+  private val transformer = new Transformer {
+    println("Encoding a Scalaz NonEmptyList.")
+
+    def transform(o: AnyRef): AnyRef = o match {
+      case d: NonEmptyList[_] => d.list
+      case _ => o
+    }
+
+  }
+
+  override def register() = {
+    println("Hooking up Scalaz serializer.")
+    /** Encoding hook for MongoDB To be able to persist Scalaz DateTime to MongoDB */
+    BSON.addEncodingHook(encodeType, transformer)
+    super.register()
+  }
+
+  override def unregister() = {
+    println("De-registering Scalaz serializer.")
+    BSON.removeEncodingHooks(encodeType)
+    super.unregister()
+  }
+}
+
+trait ScalazDeserializer extends MongoConversionHelper {
+
+  private val encodeType = classOf[List[_]]
+  private val transformer = new Transformer {
+    println("Decoding Scalaz NonEmptyList.")
+
+    def transform(o: AnyRef): AnyRef = o match {
+      case x :: xs => NonEmptyList(x, xs)
+      case _ => o
+    }
+  }
+
+  override def register() = {
+    println("Hooking up Joda DateTime deserializer")
+    /** Encoding hook for MongoDB To be able to read Scalaz DateTime from MongoDB's BSON Date */
+    BSON.addDecodingHook(encodeType, transformer)
+    super.register()
+  }
+
+  override def unregister() = {
+    println("De-registering Joda DateTime deserializer.")
+    BSON.removeDecodingHooks(encodeType)
+    super.unregister()
   }
 }
