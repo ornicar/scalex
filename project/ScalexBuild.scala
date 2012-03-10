8e821d001c052ffe6f9f544f44f4801ad8c8df31
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-10 23:55:00 +0100

    Rewrite the build

--- project/ScalexBuild.scala
+++ project/ScalexBuild.scala
@@ -1,24 +1,14 @@
 import sbt._
 import Keys._
 
-trait BuildSettings {
-  val buildOrganization = "com.github.ornicar"
-  val buildVersion = "0.2"
-  val buildScalaVersion = "2.9.1"
-
-  val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := buildOrganization,
-    version := buildVersion,
-    scalaVersion := buildScalaVersion,
-    shellPrompt := ShellPrompt.buildShellPrompt,
-    scalacOptions := Seq("-deprecation", "-unchecked"))
-}
-
 trait Resolvers {
+  val codahale = "repo.codahale.com" at "http://repo.codahale.com/"
   val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
+  val typesafeS = "typesafe.com snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
   val iliaz = "iliaz.com" at "http://scala.iliaz.com/"
-  val novus = "repo.novus snaps" at "http://repo.novus.com/snapshots/"
-  val sonatype = "sonatype releases" at "http://oss.sonatype.org/content/repositories/releases/"
+  val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
+  val sonatypeS = "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
+  val novusS = "repo.novus snaps" at "http://repo.novus.com/snapshots/"
 }
 
 trait Dependencies {
@@ -29,29 +19,27 @@ trait Dependencies {
   val slf4jNop = "org.slf4j" % "slf4j-nop" % "1.6.4"
   val paginator = "com.github.ornicar" %% "paginator-core" % "1.4"
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
-  val scalatra = "org.scalatra" %% "scalatra" % "2.0.3"
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-  val jetty = "org.eclipse.jetty" % "jetty-webapp" % "8.0.4.v20111024" % "container"
   val hasher = "com.roundeights" % "hasher" % "0.3" from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
   val sbinary = "org.scala-tools.sbinary" %% "sbinary" % "0.4.1-SNAPSHOT"
   val scalalib = "com.github.ornicar" %% "scalalib" % "1.21"
 }
 
-object ScalexBuild extends Build with BuildSettings with Resolvers with Dependencies {
-  lazy val core = Project("core", file("core"),
-    settings = buildSettings ++ Seq(
-      name := "scalex-core",
-      resolvers := Seq(typesafe, iliaz, novus, sonatype),
-      libraryDependencies ++= Seq(specs2, scalalib, casbah, salat, compiler, paginator, scalaz, hasher, sbinary, slf4jNop)))
+object ScalexBuild extends Build with Resolvers with Dependencies {
 
-  lazy val http = Project("http", file("http"),
-    settings = buildSettings ++ Seq(
-      name := "scalex-http",
-      resolvers := Seq(novus, iliaz),
-      libraryDependencies ++= Seq(scalatra, servlet, jetty, slf4jNop))) dependsOn "core"
-}
+  private val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "com.github.ornicar",
+    version := "1.0",
+    scalaVersion := "2.9.1",
+    libraryDependencies := Seq(scalalib),
+    libraryDependencies in test := Seq(specs2),
+    resolvers := Seq(typesafe, iliaz, novusS, sonatype),
+    shellPrompt := {
+      (state: State) ⇒ "%s> ".format(Project.extract(state).currentProject.id)
+    },
+    scalacOptions := Seq("-deprecation", "-unchecked")
+  )
 
-object ShellPrompt {
-  val buildShellPrompt =
-    (state: State) ⇒ "scalex:%s> ".format(Project.extract(state).currentProject.id)
+  lazy val core = Project("core", file("core"), settings = buildSettings).settings(
+    libraryDependencies ++= Seq(casbah, salat, compiler, paginator, scalaz, hasher, sbinary, slf4jNop)
+  )
 }
