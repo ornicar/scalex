2b1ae1ecf132fc217e2582e65a2b3609f63ef217
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 10:46:31 +0100

    Must specify config path when running a command

--- core/src/main/scala/Cli.scala
+++ core/src/main/scala/Cli.scala
@@ -9,46 +9,50 @@ import scalaz.Scalaz.{ success, failure }
 
 object Cli {
 
-  def env = Env("scalex.conf")
-
   def main(args: Array[String]): Unit = sys exit {
     println(args.toList match {
-      case Nil             ⇒ "No command given."
-      case command :: args ⇒ process(command, args)
+      case config :: command :: args ⇒ {
+        val env = Env(config)
+        EnvCli(env)(command, args)
+      }
+      case _ ⇒ "Usage: run /path/to/scalex.conf command args"
     })
     0
   }
 
-  def process(command: String, args: List[String]) = command match {
-    case "dump"   ⇒ dump(args)
-    case "index"  ⇒ index()
-    case "search" ⇒ search(args mkString " ", 3)
-    case "all"    ⇒ search(args mkString " ", 10000)
-    case command  ⇒ "Unknown command " + command
+  case class EnvCli(env: Env) {
+
+    def apply(command: String, args: List[String]) = command match {
+      case "dump"   ⇒ dump(args)
+      case "index"  ⇒ index()
+      case "search" ⇒ search(args mkString " ", 3)
+      case "all"    ⇒ search(args mkString " ", 10000)
+      case command  ⇒ "Unknown command " + command
+    }
+
+    def search(query: String, nb: Int): String =
+      (env.engine find RawQuery(query, 1, nb)).fold(identity, {
+        case Results(paginator, defs) ⇒
+          "%d results for %s\n\n%s\n" format (paginator.nbResults, query, render(defs))
+      })
+
+    def dump(fs: List[String]): String = {
+      val pack = fs.head
+      val files = fs.tail map (f ⇒ new File(f))
+      val sources = (new Locator) locate files map (_.getPath)
+      env.dumper.process(pack, sources)
+      "Dump complete"
+    }
+
+    def index(): String = {
+      env.indexRepo write (env.defRepo.findAll map (_.toIndex))
+      "Index complete"
+    }
+
+    private def render(d: Def): String =
+      "[" + d.pack + "] " + d.name + "\n  " + d.toString + "\n  " + d.encodedDocUrl
+
+    private def render(ds: Seq[Def]): String =
+      ds map render map ("* "+) mkString "\n\n"
   }
-
-  def search(query: String, nb: Int): String =
-    (env.engine find RawQuery(query, 1, nb)).fold(identity, {
-      case Results(paginator, defs) ⇒
-        "%d results for %s\n\n%s\n" format (paginator.nbResults, query, render(defs))
-    })
-
-  def dump(fs: List[String]): String = {
-    val pack = fs.head
-    val files = fs.tail map (f ⇒ new File(f))
-    val sources = (new Locator) locate files map (_.getPath)
-    env.dumper.process(pack, sources)
-    "Dump complete"
-  }
-
-  def index(): String = {
-    env.indexRepo write (env.defRepo.findAll map (_.toIndex))
-    "Index complete"
-  }
-
-  private def render(d: Def): String =
-    "[" + d.pack + "] " + d.name + "\n  " + d.toString + "\n  " + d.encodedDocUrl
-
-  private def render(ds: Seq[Def]): String =
-    ds map render map ("* "+) mkString "\n\n"
 }
