caec2d2607a9949c77a325c18ed08f2629000964
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-14 18:15:58 +0200

    start json storage

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ trait Resolvers {
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
   val sonatypeS = "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
   val sonatypeP = "sonatype-public" at "https://oss.sonatype.org/content/groups/public"
+  val mandubianS = "Mandubian snapshots" at "http://rawgithub.com/mandubian/mandubian-mvn/master/snapshots/"
 }
 
 trait Dependencies {
@@ -18,6 +19,7 @@ trait Dependencies {
   val scalalib = "com.github.ornicar" % "scalalib_2.10" % "3.3"
   val config = "com.typesafe" % "config" % "1.0.1"
   val scopt = "com.github.scopt" % "scopt_2.10" % "3.0.0"
+  val json = "play" % "play-json_2.10" % "2.2-SNAPSHOT"
 }
 
 object ScalexBuild extends Build with Resolvers with Dependencies {
@@ -29,11 +31,11 @@ object ScalexBuild extends Build with Resolvers with Dependencies {
     scalaVersion := "2.11.0-M3",
     libraryDependencies := Seq(config, scalalib),
     // libraryDependencies in test := Seq(specs2),
-    resolvers := Seq(typesafe, typesafeS, iliaz, sonatype, sonatypeS, sonatypeP),
+    resolvers := Seq(typesafe, typesafeS, iliaz, sonatype, sonatypeS, sonatypeP, mandubianS),
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_")
   ) //++ ScalexSbtPlugin.defaultSettings
 
   lazy val scalex = Project("scalex", file("."), settings = buildSettings).settings(
-    libraryDependencies ++= Seq(compiler, scalaz, scalazContrib, sbinary, scopt)
+    libraryDependencies ++= Seq(compiler, scalaz, scalazContrib, json, scopt)
   )
 }
--- src/main/scala/index/Storage.scala
+++ src/main/scala/index/Storage.scala
@@ -1,29 +1,19 @@
 package ornicar.scalex
 package index
 
+import play.api.libs.json._
 import java.io._
-import sbinary._
-import Operations._
 
 import model.Database
 
-object IndexRepo extends DefaultProtocol {
+object IndexRepo {
 
-  implicit val dbFormat = asProduct4(Def)(Def.unapply(_).get)
+  def toJson(db: Database): JsValue = Format.database writes db
 
-	implicit object DatabaseFormat extends Format[Database]{
-		def reads(in : Input) = listFormat[DocTemplate]
-			case 0 => Bin(reads(in), reads(in))
-			case _ => Leaf(read[String](in))
-		}
+  // def read(file: File): Database = { fromFile[Database](file) }
 
-		def writes(out : Output, value : Database) = value match {
-			case Bin(left, right) => write[Byte](out, 0); writes(out, left); writes(out, right)
-			case Leaf(label) => write[Byte](out, 1); write(out, label)
-		}
-	}
+  private object Format {
 
-  def write(file: File, db: Database) { toFile(db)(file) }
-
-  def read(file: File): Database = { fromFile[Database](file) }
+    def database: Format[Database] = Json.format[Database]
+  }
 }
