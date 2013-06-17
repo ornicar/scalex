e8be1c7937fedc4cfa37ca87110cc481f49ec57b
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-17 13:31:01 +0200

    implement Functor.map2

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,19 +1,26 @@
 package ornicar
 
 import scala.util.{ Try, Success, Failure }
+import scalaz._
 
 package object scalex
-    extends scalaz.StateFunctions // Functions related to the state monad
-    with scalaz.syntax.ToTypeClassOps // syntax associated with type classes
-    with scalaz.syntax.ToDataOps // syntax associated with Scalaz data structures
-    with scalaz.std.AllInstances // Type class instances for the standard library types
-    with scalaz.std.AllFunctions // Functions related to standard library types
-    with scalaz.syntax.std.ToAllStdOps // syntax associated with standard library types
-    with scalaz.IdInstances // Identity type and instances
-    with scalaz.contrib.std.TryInstances
-    with scalaz.contrib.std.FutureInstances {
+    extends StateFunctions // Functions related to the state monad
+    with syntax.ToTypeClassOps // syntax associated with type classes
+    with syntax.ToDataOps // syntax associated with Scalaz data structures
+    with std.AllInstances // Type class instances for the standard library types
+    with std.AllFunctions // Functions related to standard library types
+    with syntax.std.ToAllStdOps // syntax associated with standard library types
+    with IdInstances // Identity type and instances
+    with contrib.std.TryInstances
+    with contrib.std.FutureInstances {
 
   type File = java.io.File
+
+  implicit final class ScalexFunctor[M[_]: Functor, A](fa: M[A]) {
+
+    def map2[N[_], B, C](f: B ⇒ C)(implicit m: A <:< N[B], f1: Functor[M], f2: Functor[N]): M[N[C]] =
+      f1.map(fa) { k ⇒ f2.map(k: N[B])(f) }
+  }
 
   implicit final class ScalexPimpAny[A](any: A) {
 
@@ -26,7 +33,7 @@ package object scalex
     def asTry(error: ⇒ Exception): Try[A] =
       oa.fold[Try[A]](Failure(error))(Success(_))
 
-    def ??[B: scalaz.Monoid](f: A ⇒ B): B = oa.fold(∅[B])(f)
+    def ??[B: Monoid](f: A ⇒ B): B = oa.fold(∅[B])(f)
   }
 
   implicit final class ScalexPimpTry[A](ta: Try[A]) {
