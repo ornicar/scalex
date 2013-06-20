e7cb8a567d9bcbeba5ae350dabb68b0fbe22bdde
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-20 21:50:53 +0200

    extract typeclass instances out of the package object

---
+++ src/main/scala/instances.scala
@@ -0,0 +1,52 @@
+package org.scalex
+
+import scala.util.{ Try, Success, Failure }
+
+import scalaz._
+
+private[scalex] object instances extends instances
+
+private[scalex] trait instances {
+
+  implicit final class ScalexFunctor[M[_]: Functor, A](fa: M[A]) {
+
+    def map2[N[_], B, C](f: B ⇒ C)(implicit m: A <:< N[B], f1: Functor[M], f2: Functor[N]): M[N[C]] =
+      f1.map(fa) { k ⇒ f2.map(k: N[B])(f) }
+  }
+
+  implicit final class ScalexPimpAny[A](any: A) {
+
+    def asTry(cond: Boolean, error: ⇒ Exception): Try[A] =
+      if (cond) Success(any) else Failure(error)
+  }
+
+  implicit final class ScalexPimpOption[A](oa: Option[A]) {
+
+    def asTry(error: ⇒ Exception): Try[A] =
+      oa.fold[Try[A]](Failure(error))(Success(_))
+
+    def ??[B: Monoid](f: A ⇒ B): B = oa.fold(∅[B])(f)
+  }
+
+  implicit final class ScalexPimpTry[A](ta: Try[A]) {
+
+    def failureEffect(f: PartialFunction[Throwable, Unit]): Try[A] = {
+      ta recover f
+      ta
+    }
+  }
+
+  /**
+   * K combinator implementation
+   * Provides oneliner side effects
+   * See http://hacking-scala.posterous.com/side-effecting-without-braces
+   */
+  implicit def ornicarAddKcombinator[A](any: A) = new {
+    def kCombinator(sideEffect: A ⇒ Unit): A = {
+      sideEffect(any)
+      any
+    }
+    def ~(sideEffect: A ⇒ Unit): A = kCombinator(sideEffect)
+    def pp: A = kCombinator(println)
+  }
+}
--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,10 +1,10 @@
 package org
 
-import scala.util.{ Try, Success, Failure }
 import scalaz._
 
 package object scalex
-    extends StateFunctions // Functions related to the state monad
+    extends instances
+    with StateFunctions // Functions related to the state monad
     with syntax.ToTypeClassOps // syntax associated with type classes
     with syntax.ToDataOps // syntax associated with Scalaz data structures
     with std.AllInstances // Type class instances for the standard library types
@@ -18,47 +18,19 @@ package object scalex
 
   implicit def execontext = scala.concurrent.ExecutionContext.Implicits.global
 
-  implicit final class ScalexFunctor[M[_]: Functor, A](fa: M[A]) {
-
-    def map2[N[_], B, C](f: B ⇒ C)(implicit m: A <:< N[B], f1: Functor[M], f2: Functor[N]): M[N[C]] =
-      f1.map(fa) { k ⇒ f2.map(k: N[B])(f) }
-  }
-
-  implicit final class ScalexPimpAny[A](any: A) {
-
-    def asTry(cond: Boolean, error: ⇒ Exception): Try[A] =
-      if (cond) Success(any) else Failure(error)
-  }
-
-  implicit final class ScalexPimpOption[A](oa: Option[A]) {
-
-    def asTry(error: ⇒ Exception): Try[A] =
-      oa.fold[Try[A]](Failure(error))(Success(_))
-
-    def ??[B: Monoid](f: A ⇒ B): B = oa.fold(∅[B])(f)
-  }
-
-  implicit final class ScalexPimpTry[A](ta: Try[A]) {
-
-    def failureEffect(f: PartialFunction[Throwable, Unit]): Try[A] = {
-      ta recover f
-      ta
-    }
-  }
-
-  /**
-   * K combinator implementation
-   * Provides oneliner side effects
-   * See http://hacking-scala.posterous.com/side-effecting-without-braces
-   */
-  implicit def ornicarAddKcombinator[A](any: A) = new {
-    def kCombinator(sideEffect: A ⇒ Unit): A = {
-      sideEffect(any)
-      any
-    }
-    def ~(sideEffect: A ⇒ Unit): A = kCombinator(sideEffect)
-    def pp: A = kCombinator(println)
-  }
-
   def badArg(msg: String) = new IllegalArgumentException(msg)
+
+  object makeTimeout {
+
+    import akka.util.Timeout
+    import scala.concurrent.duration._
+
+    implicit val short = seconds(1)
+    implicit val large = seconds(5)
+    implicit val veryLarge = minutes(10)
+
+    def apply(duration: FiniteDuration) = Timeout(duration)
+    def seconds(s: Int): Timeout = Timeout(s.seconds)
+    def minutes(m: Int): Timeout = Timeout(m.minutes)
+  }
 }
