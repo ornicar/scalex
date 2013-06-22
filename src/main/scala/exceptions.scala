ccc12ec8025d8cfc6d1c6b321b27e1e274be057a
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-22 13:14:12 +0200

    add scalex exceptions (for Try)

---
+++ src/main/scala/exceptions.scala
@@ -0,0 +1,19 @@
+package org.scalex
+
+sealed trait ScalexException
+
+final class InvalidProjectNameException(name: String)
+  extends Exception("Invalid project name: " + name)
+  with ScalexException
+
+final class InvalidProjectVersionException(version: String)
+  extends Exception("Invalid project version: " + version)
+  with ScalexException
+
+final class OutdatedDatabaseException(name: String)
+  extends Exception("The database %s is too old and must be rebuilded" format name)
+  with ScalexException
+
+final class BadArgumentException(msg: String)
+  extends IllegalArgumentException(msg)
+  with ScalexException
