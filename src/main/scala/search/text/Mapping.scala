0e842f5e7ac21431f83530c94fb36da1f991c2db
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-23 15:56:39 +0200

    template and def mapping

--- src/main/scala/search/text/Mapping.scala
+++ src/main/scala/search/text/Mapping.scala
@@ -90,28 +90,51 @@ private[text] object Mapping extends org.scalex.util.ScalexJson {
           hi = tp str f.hi
         } yield TypeParam(name, typeParams, variance, lo, hi)
       }
+    def readTypeParams(js: JsValue): List[TypeParam] =
+      (js arr f.typeParams) ?? { _.value.toList.map(readTypeParam).flatten }
+
+    def readValueParam(js: JsValue): Option[ValueParam] =
+      js.asOpt[JsObject] flatMap { tp ⇒
+        for {
+          name ← tp str f.name
+          resultType ← tp str f.resultType
+          defaultValue = tp str f.defaultValue
+          isImplicit = (tp boolean f.isImplicit) | false
+        } yield ValueParam(name, resultType, defaultValue, isImplicit)
+      }
 
     json.asOpt[JsObject] flatMap { doc ⇒
-      for {
-        member ← doc obj f.member flatMap { member ⇒
-          for {
-            project ← Project(projectName).toOption
-            parent ← member obj f.parent flatMap { parent ⇒
-              for {
-                entity ← parent str f.entity map Entity
-                role ← parent str f.role map Role.fromString
-                typeParams = (parent arr f.typeParams) ?? {
-                  _.value.toList map readTypeParam
+      doc obj f.member flatMap { m ⇒
+        for {
+          project ← Project(projectName).toOption
+          parent ← m obj f.parent flatMap { parent ⇒
+            for {
+              entity ← parent str f.entity map Entity
+              role ← parent str f.role map Role.fromString
+              typeParams = readTypeParams(parent)
+            } yield Parent(entity, role, typeParams)
+          }
+          entity = Entity(id)
+          role ← m str f.role map Role.fromString
+          flags = (m arr f.flags) ?? { ~_.asOpt[List[String]] }
+          resultType ← m str f.resultType
+        } yield Member(project, parent, entity, role, flags, resultType)
+      } map { member ⇒
+        member.role match {
+          case Role.Def ⇒ Def(
+            member,
+            readTypeParams(doc),
+            (doc arr f.valueParams) ?? {
+              _.value.toList map {
+                _.asOpt[JsArray] ?? {
+                  _.value.toList.map(readValueParam).flatten
                 }
-              } yield Parent(entity, role, typeParams.flatten)
-            }
-            entity = Entity(id)
-            role ← member str f.role map Role.fromString
-            flags = (member arr f.flags) ?? { ~_.asOpt[List[String]] }
-            resultType ← member str f.resultType
-          } yield Member(project, parent, entity, role, flags, resultType)
+              }
+            })
+          case Role.Val | Role.LazyVal | Role.Var ⇒ Val(member)
+          case _                                  ⇒ Template(member, readTypeParams(doc))
         }
-      } yield Val(member)
+      }
     }
   }
 }
