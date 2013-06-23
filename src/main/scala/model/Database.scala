a846fdbcab04fe1a91fc4c3f2534f8f3b9b59a85
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-23 15:36:11 +0200

    remove database merge facilities

--- src/main/scala/model/Database.scala
+++ src/main/scala/model/Database.scala
@@ -14,12 +14,4 @@ case class Database(seeds: List[Seed]) {
   def header = Header(projects)
 
   def describe: String = seeds map (_.describe) mkString "\n\n\n"
-
-  def merge(other: Database) = Database((seeds ++ other.seeds).distinct)
-}
-
-object Database {
-
-  def merge(dbs: List[Database]): Database = 
-    dbs.toNel.fold(Database(Nil)) { _ foldLeft1 { _ merge _ } }
 }
